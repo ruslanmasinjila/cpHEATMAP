fefd15af0822f8961d308dcba6da826f4e73c75d
Ruslan Masinjila <ruslanmasinjila@gmail.com>
2019-11-30 01:59:22 +0300

    extracted and tabulated closing rates and times for each currency pair

--- cpHEATMAP.py
+++ cpHEATMAP.py
@@ -2,8 +2,13 @@ from datetime import datetime
 from MetaTrader5 import *
 from pytz import timezone
 import matplotlib.pyplot as plt
+import pandas as pd
 
- 
+
+#==================================================
+#INITIALIZATION OF MT5 TERMINAL
+#==================================================
+
 # connect to MetaTrader 5
 # Log into MetaTrader if necessary
 MT5Initialize()
@@ -20,6 +25,11 @@ print(MT5TerminalInfo())
 # display data on MetaTrader 5 version
 print(MT5Version())
 
+
+#==================================================
+# GETTING RATES FROM MT5 TERMINAL
+#==================================================
+
 # Read currency pair names from the file into a list
 currency_pairs = [currency_pair.rstrip('\n') for currency_pair in open("currency_pairs.txt")]
 
@@ -35,12 +45,32 @@ for i in currency_pairs:
 	rates.append(MT5CopyRatesFrom(i, MT5_TIMEFRAME_H1, utc_from, N))
 	  
 	
+#==================================================
+# SHUTTING DOWN MT5 TERMINAL
+#==================================================
 
 # shut down connection to the MetaTrader 5 terminal
 MT5Shutdown()
 
-for i in range(len(rates[0])):
-	print(rates[0][i][0])
-	print(rates[15][i][0])
-	print("=================")
+
+#==================================================
+# PROCESSING DATA FROM MT5 TERMINAL
+#==================================================
+
+# Initialization of pandas dataframe for currency pairs and their closing prices
+cpTABLE = pd.DataFrame(index = currency_pairs, columns = [i[0] for i in rates[0]])
+
+
+# POPULATE THE TABLE
+for i in range(len(rates)):			# For every currency pair
+
+	closing_prices = []
+	for j in rates[i]:		    	# For every rate in the current currency pair
+		closing_prices.append(j[4])	# Closing price is at index 4 of every rate
+	
+	cpTABLE.loc[currency_pairs[i],:] = closing_prices
+
+
+print(cpTABLE)
+
 
--- readme.txt
+++ readme.txt
@@ -2,13 +2,14 @@ INSTALLATION
 - pip install MetaTrader5
 - pip install matplotlib
 - pip install pytz
+- pip install pandas
 
 
 TODO:
 - Starting from current UTC time, get N rates for currency pairs from DarwinEx Server using MetaTrader5 API...[DONE]
 
 
-- For each currency pair, extract the time and the corresponding closing price. Enter the information in pandas table
+- For each currency pair, extract the time and the corresponding closing price. Enter the information in pandas table...[DONE]
 
  -------------- ----------------------------------------
 |currency_pair | time1 | time2 | time3 | time4 | timeN  |
@@ -23,6 +24,9 @@ TODO:
 ---------------------------------------------------------
 | pairN        |       |       |       |       |        |
 ---------------------------------------------------------
+
+
+- Scale the prices for each currency pair in the table across time between 0 and 1
 
  
 - Generate heatmap for the prices
